c1f4c1ce4d7b12979fd101fff23553afee31dc5d
LuisGod <godinezluis1990@gmail.com>
2015-11-08 08:09:35 -0600

    LUIS 01
    YA SE LE ARREGLO LO DE  LA CONEXION

---
+++ PRIMER_PROYECTO_UAM-2/PRIMER_PROYECTO_UAM/ClienteController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PRIMER_PROYECTO_UAM
+{
+   public   class ClienteController
+    {
+      //Conexion myconexion = new Conexion();
+      
+       //SqlConnection miconexion = new SqlConnection(Conexion.sqlConnection);
+       
+       //SqlCommand comando = myconexion.CreateCommand(conexion);
+
+     
+
+       private string buscar;
+        public string Buscar
+        {
+            get { return buscar; }
+            set { buscar = value; }
+        }
+
+        public void listarclientes(DataGridView data)
+       {
+           Conexion myconexion = new Conexion();
+           SqlConnection conexion = myconexion.CreateConnection();
+           //SqlCommand comando = myconexion.CreateCommand(conexion);
+           
+           conexion.Open();
+           SqlCommand comando = new SqlCommand("SELECT * FROM CLIENTE", conexion);
+           comando.Connection = conexion;
+           comando.ExecuteNonQuery();
+           DataTable dt = new DataTable();
+           SqlDataAdapter da = new SqlDataAdapter(comando);
+           da.Fill(dt);
+           data.DataSource = dt;
+           data.Columns[0].Width = 60;
+           data.Columns[1].Width = 165;
+           data.Columns[2].Width = 165;
+           data.Columns[3].Width = 90;
+           data.Columns[4].Width = 50;
+           data.Columns[5].Width = 165;
+           data.Columns[6].Width = 100;
+           data.Columns[7].Width = 125;
+
+           conexion.Close();
+       }
+       public void buscarcedula(DataGridView data)
+        {
+            Conexion myconexion = new Conexion();
+            SqlConnection conexion = myconexion.CreateConnection();
+           conexion.Open();
+           SqlCommand comando = new SqlCommand("SELECT * FROM CLIENTE where DNI like ('" + buscar + "%')",conexion);
+           comando.Connection = conexion;
+           comando.ExecuteNonQuery();
+           DataTable dt = new DataTable();
+           SqlDataAdapter da = new SqlDataAdapter(comando);
+           da.Fill(dt);
+           data.DataSource = dt;
+           conexion.Close();
+       }
+
+       public void buscarapellido(DataGridView data)
+       {
+           Conexion myconexion = new Conexion();
+           SqlConnection conexion = myconexion.CreateConnection();
+           conexion.Open();
+           SqlCommand comando = new SqlCommand("SELECT * FROM CLIENTE where APELLIDOS like ('" + buscar + "%')", conexion);
+           comando.Connection = conexion;
+           comando.ExecuteNonQuery();
+           DataTable dt = new DataTable();
+           SqlDataAdapter da = new SqlDataAdapter(comando);
+           da.Fill(dt);
+           data.DataSource = dt;
+           conexion.Close();
+       }
+       public void eliminar(DataGridView dataelimina)
+       {
+           Conexion myconexion = new Conexion();
+           SqlConnection conexion = myconexion.CreateConnection();
+           DialogResult resultado = MessageBox.Show("¿Estas Seguro de Eliminar al cliente?");
+           if (resultado == DialogResult.No)
+           {
+               return;
+           }
+       
+           {
+               SqlCommand comando = new SqlCommand("BORRAR_CLIENTE", conexion);
+               comando.CommandType = CommandType.StoredProcedure;
+               comando.Parameters.AddWithValue("ID", dataelimina.CurrentRow.Cells["ID"].Value);
+               conexion.Open();
+               comando.ExecuteNonQuery();
+               conexion.Close();
+
+              MessageBox.Show("El cliente ha sido eliminado" );
+           }
+           
+       }
+
+    }
+}
---
+++ PRIMER_PROYECTO_UAM-2/PRIMER_PROYECTO_UAM/Consultar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace PRIMER_PROYECTO_UAM
+{
+    public partial class Consultar : Form
+    {
+        //Conexion myConnection = new Conexion();
+        //SqlConnection sqlConnection = myConnection.CreateConnection();
+        //SqlCommand comando = myConnection.CreateCommand(sqlConnection);
+        public Consultar()
+        {
+            InitializeComponent();
+        }
+
+        private void Consultar_Load(object sender, EventArgs e)
+        {
+            ////////////////////////////////////////////////////////////////////////////////
+            List<ProvinciaBE> listaP = new List<ProvinciaBE>();
+            ProvinciaController provinciacontrola = new ProvinciaController();
+            listaP = provinciacontrola.CONSULTAPROVINCIA();
+
+            BindingSource provinciasourse = new BindingSource();
+            provinciasourse.DataSource = listaP;
+            cmbprovincia.DataSource = provinciasourse;
+            cmbprovincia.DisplayMember = "PROVINCIA";
+            cmbprovincia.ValueMember = "IDPROVINCIAS";
+        }
+
+        private void btneditar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
